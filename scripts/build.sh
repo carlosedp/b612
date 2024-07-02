9cf9f2a7fce06c8d31e65d3b8b17fb6071b60ac1
Carlos de Paula <me@carlosedp.com>
2024-07-02 15:43:19 -0300

    Update build script and fix signature

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,33 +8,46 @@ set -euo pipefail
 readonly ARGS=$*
 FONT_TTFS=()
 FONT_VFBS=()
+FONT_UFO=()
 
-for style in "Regular" "Italic" "Bold" "BoldItalic"
-do
-   FONT_TTFS+=("../fonts/ttf/B612-$style.ttf")
-   FONT_TTFS+=("../fonts/ttf/B612Mono-$style.ttf")
-   FONT_VFBS+=("../sources/vfb/B612-$style.vfb")
-   FONT_VFBS+=("../sources/vfb/B612Mono-$style.vfb")
+for style in "Regular" "Italic" "Bold" "BoldIta"; do
+  FONT_TTFS+=("../fonts/ttf/B612MonoLigaNerdFont-$style.ttf")
+  FONT_TTFS+=("../fonts/otf/B612MonoLigaNerdFont-$style.otf")
+  FONT_UFO+=("../sources/ufo/B612MonoLigaNerdFont-$style.ufo")
 done
+
+# Check pre-reqs (gftools and silfont)
+if ! command -v gftools &>/dev/null; then
+  warn "gftools is not installed. Please install it from pip (pip install gftools)"
+  exit 1
+fi
+if ! command -v psfnormalize &>/dev/null; then
+  warn "psfnormalize is not installed. Please install it from pip (pip install silfont)"
+  exit 1
+fi
 
 # -----------------------------------------------------------------------------
 # ---- UTILS ------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 log() {
-  message=$1; shift
-  color=$1; shift
+  message=$1
+  shift
+  color=$1
+  shift
   nc="\033[0m\n"
-  printf "${color}[DEPLOY] $message$nc";
+  printf "${color}[DEPLOY] $message$nc"
 }
 
 info() {
-  message=$1; shift
+  message=$1
+  shift
   green="\033[0;32m"
   log "$message" "$green"
 }
 
 warn() {
-  message=$1; shift
+  message=$1
+  shift
   red="\033[0;31m"
   log "$message" "$red"
 }
@@ -45,26 +58,37 @@ warn() {
 
 main() {
   info "Fix font digital signature (DSIG) / Fix font GASP and PREP table"
-  
-  for ttf in ${FONT_TTFS[*]}
-  do
+
+  for ttf in ${FONT_TTFS[*]}; do
     echo $ttf
-    gftools fix-dsig --autofix $ttf
-    gftools fix-nonhinting $ttf $ttf
+    # gftools fix-dsig --autofix $ttf
+    mv $ttf $ttf-orig
+    gftools fix-nonhinting $ttf-orig $ttf
   done
 
   info "Export vfb as UFO and normalize UFO"
 
-  for vfb in ${FONT_VFBS[*]}
-  do
-    ufo=${vfb//vfb/ufo}
+  for ufo in ${FONT_UFO[*]}; do
     echo $ufo
-    vfb2ufo -fo $vfb $ufo
     psfnormalize $ufo
   done
- 
+
+  # Remove temp files
+  for ttf in ${FONT_TTFS[*]}; do
+    rm $ttf-orig
+    rm $ttf--backup-fonttools-prep-gasporig
+    rm
+  done
+
+  # for vfb in ${FONT_VFBS[*]}; do
+  #   ufo=${vfb//vfb/ufo}
+  #   echo $ufo
+  #   # vfb2ufo -fo $vfb $ufo
+  #   psfnormalize $ufo
+  # done
+
   info "Finished building B612 font"
-  exit 0;
+  exit 0
 }
 
 main
